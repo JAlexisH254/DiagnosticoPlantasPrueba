ed95daa6e70ac3ea1c79fcec9d4daf61abbeabb8
JAle <>
2020-01-06 11:08:20 -0500

    ip server

--- server.py
+++ server.py
@@ -13,4 +13,4 @@ def postHandler():
     imagefile.save(filename)
     return pred.predict(filename)
 
-app.run(host='192.168.0.111', port=8000, debug=True)
+app.run(host='127.0.0.1', port=8000, debug=True)
