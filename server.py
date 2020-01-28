bce7b7acd12d196a126474c63ce6898780784cda
JAle <>
2020-01-27 21:47:10 -0500

    cm3

--- server.py
+++ server.py
@@ -14,4 +14,5 @@ def postHandler():
     return pred.predict(filename)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80, debug=True)
+    # app.run(host='127.0.0.1', port=80, debug=True)
+    app.run()
